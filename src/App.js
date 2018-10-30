670070aaeb4bee7b19d9acaa4f58f4d2d11c9ac1
Lucille R. Tibbetts <tibbetts.l@husky.neu.edu>
2018-10-29 21:10:43 -0700

    moved nav bar to root App.js to render on every page, outlined AdminPage.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 import SignUpPage from "./SignUpPage.js";
 import MainPage from "./MainPage.js";
 import LogInPage from "./LogInPage.js";
+import NavigationBar from "./NavigationBar.js";
 import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends Component {
+  // TODO: store state in cookies to persist data if a user refreshes the page
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn : false,
+      isAdmin : false
+    }
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div>
+          <NavigationBar isLoggedIn = {this.state.isLoggedIn} />
           <Route exact={true} path='/' render={() => (
             <div className="App">
               <MainPage />
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import NavigationBar from './NavigationBar.js';
 
 class MainPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn : false,
-      isAdmin : false
-    }
-  }
 
   render() {
     return (
@@ -17,7 +9,6 @@ class MainPage extends Component {
         <MuiThemeProvider>
           <div>
             <h1>Zags Abroad</h1>
-            <NavigationBar isLoggedIn = {this.state.isLoggedIn} />
           </div>
         </MuiThemeProvider>
       </div>

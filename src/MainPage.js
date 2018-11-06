a1c1c005eac0acf70502f2f083ab36fa8baf5faa
Katrina M. Baber <katrinambaber@Stark-Industries-2.local>
2018-11-05 20:21:25 -0800

    added drop down search bar to non-admin main page

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AutoComplete from 'material-ui/AutoComplete';
+
 
 class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput : '',
+      dataSource : ['CPSC', 'BUSI', 'MUSC', 'PHIL', 'MATH','ETC']
+    }
+  }
+
+
 
   render() {
     return (
@@ -9,6 +20,12 @@ class MainPage extends Component {
         <MuiThemeProvider>
           <div>
             <h1> Welcome! </h1>
+            <AutoComplete
+              hintText = 'Enter a deparment here (uppercase)'
+              dataSource={this.state.dataSource}
+              onChange={(value) => this.setState({userInput : value})}
+              onUpdateInput={() => console.log(this.state.userInput)}
+            />
           </div>
         </MuiThemeProvider>
       </div>

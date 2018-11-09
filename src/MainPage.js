084852c268530f3c7ccf26339577c2a15c77f0c4
Katrina M. Baber <katrinambaber@Stark-Industries-2.local>
2018-11-08 20:01:38 -0800

    added course table and filtering by department

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AutoComplete from 'material-ui/AutoComplete';
+import axios from 'axios';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
 
 
 class MainPage extends Component {
@@ -8,8 +14,30 @@ class MainPage extends Component {
     super(props);
     this.state = {
       userInput : '',
-      dataSource : ['CPSC', 'BUSI', 'MUSC', 'PHIL', 'MATH','ETC']
+      dataSource : ['CPSC', 'BUSI', 'MUSC', 'PHIL', 'MATH','ETC'],
+      courses : [],
+      filteredCourses :[],
+      currentCourse : '',
+      newArray : [],
+      counter : 0
     }
+    //backend API
+    axios.get("https://zagsabroad-backend.herokuapp.com/courses").then((res) => {
+      this.setState({courses: res.data});
+    });
+  }
+
+  setFilteredCourses(counter, currentCourse, array) {
+    var filteredArray = []
+    var n = this.state.userInput.length
+    while (counter < this.state.courses.length) {
+      currentCourse = this.state.courses[counter].gu_course_number.substring(0,n);
+      if (currentCourse === this.state.userInput) {
+          filteredArray = filteredArray.concat(this.state.courses[counter])
+      }
+      counter++;
+    }
+    this.setState({filteredCourses: filteredArray})
   }
 
 
@@ -23,9 +51,39 @@ class MainPage extends Component {
             <AutoComplete
               hintText = 'Enter a deparment here (uppercase)'
               dataSource={this.state.dataSource}
-              onChange={(value) => this.setState({userInput : value})}
-              onUpdateInput={() => console.log(this.state.userInput)}
+              onChange={
+                (event, value) => this.setState({userInput : event.currentTarget.value},
+                  () => this.setFilteredCourses(0, [], [])
+              )}
+              onSelect={ (event, value) => this.setState({userInput : event.currentTarget.value},
+                () => this.setFilteredCourses(0, [], [])
+            )}
             />
+            <h1> Search Results: </h1>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell> Host program </TableCell>
+                  <TableCell> Host course number </TableCell>
+                  <TableCell> Host course name </TableCell>
+                  <TableCell> GU course number </TableCell>
+                  <TableCell> GU course name </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.filteredCourses.map(course => {
+                  return(
+                    <TableRow key={this.state.filteredCourses.indexOf(course)}>
+                      <TableCell> {course.host_program} </TableCell>
+                      <TableCell> {course.host_course_number} </TableCell>
+                      <TableCell> {course.host_course_name} </TableCell>
+                      <TableCell> {course.gu_course_number} </TableCell>
+                      <TableCell> {course.gu_course_name} </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
           </div>
         </MuiThemeProvider>
       </div>

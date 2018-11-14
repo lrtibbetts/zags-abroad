35ad2fcb863f1beb32fac2dbec30d8d423861881
Lucille R. Tibbetts <lucilletibbetts@gmail.com>
2018-11-13 17:47:57 -0800

    connected to add and update apis, added tests directory

--- src/AdminPage.js
+++ src/AdminPage.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import MUIDataTable from "mui-datatables";
-// library code: https://github.com/gregnb/mui-datatables
-// npm docs: https://www.npmjs.com/package/mui-datatables
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import CourseDetailsForm from './CourseDetailsForm.js';
@@ -20,6 +18,8 @@ class AdminPage extends Component {
       courses: [],
       ids: [], // Array to store ids internally
       // Customize column properties
+      editingCourseId: '', // Id of course being edited
+      editingCourse: [], // Array with details of course being edited
       columns: [
         {
           name: "Host Program",
@@ -92,6 +92,17 @@ class AdminPage extends Component {
       ]
     }
 
+    // Bind 'this' context to helper functions
+    this.loadCourses = this.loadCourses.bind(this);
+    this.deleteRows = this.deleteRows.bind(this);
+    this.toggleAddForm = this.toggleAddForm.bind(this);
+    this.populateEditForm = this.populateEditForm.bind(this);
+    this.hideEditForm = this.hideEditForm.bind(this);
+
+    this.loadCourses();
+  }
+
+  loadCourses() {
     axios.get("https://zagsabroad-backend.herokuapp.com/courses").then((res) => {
       // Convert array of objects to 2D array
       const coursesToAdd = [];
@@ -108,13 +119,8 @@ class AdminPage extends Component {
         coursesToAdd.push(equivalency);
       }
       this.setState({courses : coursesToAdd, ids: idsToAdd});
+      console.log('courses loaded');
     });
-
-    // Bind 'this' context to helper functions
-    this.deleteRows = this.deleteRows.bind(this);
-    this.toggleEditForm = this.toggleEditForm.bind(this);
-    this.toggleAddForm = this.toggleAddForm.bind(this);
-    this.rowSelected = this.rowSelected.bind(this);
   }
 
   deleteRows(rowsToDelete) {
@@ -124,36 +130,39 @@ class AdminPage extends Component {
       var courseInfo = {
         id : id
       }
-      // Call backend API
       axios.post("https://zagsabroad-backend.herokuapp.com/deletecourse", courseInfo).then((res) => {
         console.log(res.data);
       });
     }
   }
 
-  toggleEditForm() {
-    this.setState({showEditForm : !this.state.showEditForm});
-  }
-
   toggleAddForm() {
     this.setState({showAddForm : !this.state.showAddForm});
   }
 
-  rowSelected() {
+  populateEditForm(rowData, rowMeta) {
+    // Get course details for row clicked
+    let id = this.state.ids[rowMeta.dataIndex]; // Get course id
+    this.setState({editingCourseId: id, editingCourse: rowData, showEditForm: true});
+  }
+
+  hideEditForm() {
+    // Do not display editing form if a row is selected (vs. clicked)
     this.setState({showEditForm : false});
   }
 
   render() {
+    // TODO: reload table after course is updated or added
     const cookies = this.props.cookies;
     if(cookies.get('role') === 'admin') {
       const options = {
         // Customization of data table
-        filterType: "multiselect", // Apply filters via dropdown menus
+        filterType: "multiselect", // Apply multiple filters via dropdown menus
         print: false, // Remove print icon
         downloadOptions: {filename: "Course Equivalencies.csv"}, // Custom file name
-        onRowsSelect: this.rowSelected, // Prevent editing form from popping up when row is "selected" vs. "clicked"
+        onRowClick: this.populateEditForm,
+        onRowsSelect: this.hideEditForm, // Prevent editing form from popping up when row is "selected" vs. "clicked"
         onRowsDelete: this.deleteRows,
-        onRowClick: this.toggleEditForm,
         rowsPerPage: 10, // Default to 20 rows per page
         rowsPerPageOptions: [10, 50, 100],
         fixedHeader: false, // Headers will move if the user scrolls across the table
@@ -172,11 +181,13 @@ class AdminPage extends Component {
                 data = {this.state.courses}
                 options = {options}/>
               {this.state.showAddForm === true ? <CourseDetailsForm
+                course={[]} // Adding a new course, so pass an empty array
                 onClose={this.toggleAddForm}
                 title="Add Course Equivalency"/> : null}
               {this.state.showEditForm === true ? <CourseDetailsForm
-                // TODO: pass current course details as props
-                onClose={this.toggleEditForm}
+                courseId={this.state.editingCourseId}
+                course={this.state.editingCourse}
+                onClose={this.hideEditForm}
                 title="Edit Course Equivalency"/> : null}
             </div>
           </MuiThemeProvider>
--- src/CourseDetailsForm.js
+++ src/CourseDetailsForm.js
@@ -4,30 +4,94 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from 'material-ui/TextField';
+import axios from 'axios';
 
 class CourseDetailForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      host_program: this.props.course[0],
+      host_course_number: this.props.course[1],
+      host_course_name: this.props.course[2],
+      gu_course_number: this.props.course[3],
+      gu_course_name: this.props.course[4],
+      comments: this.props.course[5],
+      signature_needed: this.props.course[6],
+      approved_by: this.props.course[7],
+      approval_date: this.props.course[8],
+      approved_until: this.props.course[9],
+      department: this.props.course[10]
+    }
+  }
 
   render() {
+    // TODO: confirmation message when course is added or updated successfully
+    // TODO: autofilled dropdown menus for program, department
+    // TODO: disable save button for add form until required fields are filled
     return (
       <div>
         <MuiThemeProvider>
           <Dialog open={true} onClose={this.props.onClose} scroll='body'>
             <DialogTitle  id="simple-dialog-title"> {this.props.title} </DialogTitle>
             <div>
-            <TextField floatingLabelText = "Host program"/>
-            <TextField floatingLabelText = "Host course number"/>
-            <TextField floatingLabelText = "Host course name"/>
-            <TextField floatingLabelText = "GU course number"/>
-            <TextField floatingLabelText = "GU course name"/>
-            <TextField floatingLabelText = "Comments"/>
-            <TextField floatingLabelText = "Signature needed"/>
-            <TextField floatingLabelText = "Approved by"/>
-            <TextField floatingLabelText = "Approval date"/>
-            <TextField floatingLabelText = "Approved until"/>
-            <TextField floatingLabelText = "Department"/> <br/>
+            <TextField floatingLabelText = "Host program"
+              defaultValue = {this.state.host_program}
+              onChange = { (event, newValue) =>
+                this.setState({host_program : newValue})}/>
+            <TextField floatingLabelText = "Host course number"
+              defaultValue = {this.state.host_course_number}
+              onChange = { (event, newValue) =>
+                this.setState({host_course_number : newValue})}/>
+            <TextField floatingLabelText = "Host course name"
+              defaultValue = {this.state.host_course_name}
+              onChange = { (event, newValue) =>
+                this.setState({host_course_name : newValue})}/>
+            <TextField floatingLabelText = "GU course number"
+              defaultValue = {this.state.gu_course_number}
+              onChange = { (event, newValue) =>
+                this.setState({gu_course_number : newValue})}/>
+            <TextField floatingLabelText = "GU course name"
+              defaultValue = {this.state.gu_course_name}
+              onChange = { (event, newValue) =>
+                this.setState({gu_course_name : newValue})}/>
+            <TextField floatingLabelText = "Comments"
+              defaultValue = {this.state.comments}
+              onChange = { (event, newValue) =>
+                this.setState({comments : newValue})}/>
+            <TextField floatingLabelText = "Signature needed"
+              defaultValue = {this.state.signature_needed}
+              onChange = { (event, newValue) =>
+                this.setState({signature_needed : newValue})}/>
+            <TextField floatingLabelText = "Approved by"
+              defaultValue = {this.state.approved_by}
+              onChange = { (event, newValue) =>
+                this.setState({approved_by : newValue})}/>
+            <TextField floatingLabelText = "Approval date"
+              defaultValue = {this.state.approval_date}
+              onChange = { (event, newValue) =>
+                this.setState({approval_date : newValue})}/>
+            <TextField floatingLabelText = "Approved until"
+              defaultValue = {this.state.approved_until}
+              onChange = { (event, newValue) =>
+                this.setState({approved_until : newValue})}/>
+            <TextField floatingLabelText = "Department"
+              defaultValue = {this.state.department}
+              onChange = { (event, newValue) =>
+                this.setState({department : newValue})}/>
+            <br/>
             <RaisedButton label="Save"
               onClick = {(event) => {
-                // TODO: call backend API to update or save based on type of form
+                let courseInfo = this.state;
+                if(this.props.title === "Add Course Equivalency") {
+                  axios.post("https://zagsabroad-backend.herokuapp.com/addcourse", courseInfo).then((res) => {
+                      console.log(res.data);
+                  });
+                } else if(this.props.title === "Edit Course Equivalency") {
+                  courseInfo.id = this.props.courseId; // Add course id to courseInfo object
+                  axios.post("https://zagsabroad-backend.herokuapp.com/editcourse", courseInfo).then((res) => {
+                      console.log(res.data);
+                  });
+                }
                 this.props.onClose();
               }}/>
             <RaisedButton label="Cancel"

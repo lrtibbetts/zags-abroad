11af7251b3be3868175549c30c4eb14d11a653f1
Katrina M. Baber <katrinambaber@Stark-Industries-2.local>
2018-11-13 20:10:28 -0800

    added search functionality

--- src/MainPage.js
+++ src/MainPage.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AutoComplete from 'material-ui/AutoComplete';
 import axios from 'axios';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Chip from '@material-ui/core/Chip';
+import Paper from '@material-ui/core/Paper';
+import Button from "@material-ui/core/Button";
+
 
 
 class MainPage extends Component {
@@ -17,9 +26,12 @@ class MainPage extends Component {
       dataSource : ['CPSC', 'BUSI', 'MUSC', 'PHIL', 'MATH','ETC'],
       courses : [],
       filteredCourses :[],
-      currentCourse : '',
       newArray : [],
-      counter : 0
+      programList : [],
+      filters : null,
+      selectedIndex : 1,
+      options : ['Search options:','Course Deparment','Language','Location'],
+      listOfFilters : []
     }
     //backend API
     axios.get("https://zagsabroad-backend.herokuapp.com/courses").then((res) => {
@@ -27,19 +39,70 @@ class MainPage extends Component {
     });
   }
 
-  setFilteredCourses(counter, currentCourse, array) {
+  setFilteredCourses(counter, currentC, array) {
     var filteredArray = []
-    var n = this.state.userInput.length
+    var filteredPrograms = []
     while (counter < this.state.courses.length) {
-      currentCourse = this.state.courses[counter].gu_course_number.substring(0,n);
-      if (currentCourse === this.state.userInput) {
+      var currentCourse = this.state.courses[counter].gu_course_number.substring(0,4);
+      for (var i = 0; i < this.state.listOfFilters.length; i++) {
+        if (currentCourse === this.state.listOfFilters[i]) {
           filteredArray = filteredArray.concat(this.state.courses[counter])
+          filteredPrograms = filteredPrograms.concat(this.state.courses[counter].host_program)
+        }
+      }
+      if (currentCourse === this.state.userInput) {
+
       }
       counter++;
     }
     this.setState({filteredCourses: filteredArray})
+    this.setState(
+      {programList : filteredPrograms},
+      () => this.setProgramList()
+    )
   }
 
+  setProgramList() {
+    var newAr = []
+    for (var i = 0; i < this.state.programList.length; i++) {
+      if (this.state.programList[i] !== this.state.programList[i + 1]) {
+        newAr = newAr.concat(this.state.programList[i])
+      }
+    }
+    this.setState(
+      {programList : newAr}
+    )
+  }
+
+  handleClickOnMenu = (event, index) => {
+    this.setState({filters : null, selectedIndex : index})
+  }
+
+  handleClickOnList = event => {
+    this.setState({
+      filters : event.currentTarget,
+    })
+  }
+
+  handleClose = () => {
+    this.setState({filters : null})
+  }
+
+  setFilters() {
+    var arr = this.state.listOfFilters
+    arr = arr.concat(this.state.userInput)
+    this.setState({listOfFilters : arr})
+  }
+
+  handleDelete = (filter) => {
+    var array = []
+    for (var i = 0; i < this.state.listOfFilters.length; i++) {
+      if (filter !== this.state.listOfFilters[i]) {
+        array = array.concat(this.state.listOfFilters[i])
+      }
+    }
+    this.setState({listOfFilters : array})
+}
 
 
   render() {
@@ -47,43 +110,73 @@ class MainPage extends Component {
       <div>
         <MuiThemeProvider>
           <div>
-            <h1> Welcome! </h1>
+            <h1> Welcome to the main program search page! </h1>
+            <List component="nav">
+              <ListItem
+                button
+                aria-haspopup="true"
+                aria-controls="lock-menu"
+                aria-label="Search By:"
+                onClick={this.handleClickOnList}
+                >
+                <ListItemText
+                  primary="Search By:"
+                  secondary={this.state.options[this.state.selectedIndex]}
+                />
+              </ListItem>
+            </List>
+            <Menu
+              id="lock-menu"
+              filters={this.state.filters}
+              open={Boolean(this.state.filters)}
+              onClose={this.handleClose}
+              >
+              {this.state.options.map((option, index) => (
+                <MenuItem
+                  key={option}
+                  disabled={index === 0}
+                  selected={index === this.state.selectedIndex}
+                  onClick={event => this.handleClickOnMenu(event, index)}
+                  >
+                  {option}
+                  </MenuItem>
+              ))}
+            </Menu>
+
+
             <AutoComplete
               hintText = 'Enter a deparment here (uppercase)'
               dataSource={this.state.dataSource}
-              onChange={
-                (event, value) => this.setState({userInput : event.currentTarget.value},
-                  () => this.setFilteredCourses(0, [], [])
-              )}
-              onSelect={ (event, value) => this.setState({userInput : event.currentTarget.value},
+              onMouseOut ={ (event, value) => this.setState({userInput : event.currentTarget.value},
                 () => this.setFilteredCourses(0, [], [])
-            )}
+              )}
             />
+
+            <Button color="primary" onClick ={this.setFilters.bind(this)}>
+              Add Filter
+            </Button>
+
+
+            <Paper>
+              {this.state.listOfFilters.map(data => {
+                let icon = null;
+                return (
+                  <Chip
+                    icon={icon}
+                    label={data}
+                  />
+                );
+              })}
+            </Paper>
+
             <h1> Search Results: </h1>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell> Host program </TableCell>
-                  <TableCell> Host course number </TableCell>
-                  <TableCell> Host course name </TableCell>
-                  <TableCell> GU course number </TableCell>
-                  <TableCell> GU course name </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {this.state.filteredCourses.map(course => {
+                {this.state.programList.map((program, i) => {
                   return(
-                    <TableRow key={this.state.filteredCourses.indexOf(course)}>
-                      <TableCell> {course.host_program} </TableCell>
-                      <TableCell> {course.host_course_number} </TableCell>
-                      <TableCell> {course.host_course_name} </TableCell>
-                      <TableCell> {course.gu_course_number} </TableCell>
-                      <TableCell> {course.gu_course_name} </TableCell>
-                    </TableRow>
-                  );
+                    <li>
+                      The program {program} is offered with this criteria.
+                    </li>
+                  )
                 })}
-              </TableBody>
-            </Table>
           </div>
         </MuiThemeProvider>
       </div>

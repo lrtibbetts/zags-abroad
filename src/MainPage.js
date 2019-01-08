ef8b1a9dad2a3ce601e2fd688bebdedb1bc70ca7
Lucille R. Tibbetts <lucilletibbetts@gmail.com>
2019-01-07 19:01:22 -0800

    update filtering to and relation instead of or

--- src/MainPage.js
+++ src/MainPage.js
@@ -6,7 +6,6 @@ import DropdownTextField from './DropdownTextField.js';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -30,11 +29,12 @@ class MainPage extends Component {
       programList : [] // Programs matching a user's search
     }
 
-    axios.get("https://zagsabroad-backend.herokuapp.com/subjects").then((res) => {
+    axios.get("http://localhost:3001/subjects").then((res) => {
       let subjectsToAdd = [];
       for(let i = 0; i < res.data.length; i++) {
         let subjectName = res.data[i].subject_name.trim(); // Remove any white space
-        let subjectObj = {value: subjectName, label: subjectName};
+        let subjectCode = res.data[i].subject_code.trim();
+        let subjectObj = {value: subjectCode, label: subjectName};
         subjectsToAdd.push(subjectObj);
       }
       this.setState({subjects: subjectsToAdd});
@@ -45,7 +45,7 @@ class MainPage extends Component {
   handleDeleteFilter = filter => () => {
     var filters = this.state.listOfFilters;
     for(var i = 0; i < filters.length; i++) {
-      if (filter === filters[i]) {
+      if (filter.value === filters[i].value) {
         filters.splice(i, 1);
         this.setState({listOfFilters: filters});
       }
@@ -53,8 +53,8 @@ class MainPage extends Component {
     this.getPrograms(); // Update program results based on new state
     for(var j = 0; j < this.state.subjects.length; j++) {
       var subjects = this.state.subjects;
-      if(filter < subjects[j].value) {
-        subjects.splice(j, 0, {value: filter, label: filter}); // Insert at j, remove 0 items
+      if(filter.value < subjects[j].value) {
+        subjects.splice(j, 0, filter); // Insert at j, remove 0 items
         this.setState({subjects: subjects});
         return;
       }
@@ -64,7 +64,7 @@ class MainPage extends Component {
   // Remove subject from dropdown menu after it is selected
   handleDeleteSubject(subject) {
     for (var i = 0; i < this.state.subjects.length; i++) {
-      if (subject === this.state.subjects[i].value) {
+      if (subject.value === this.state.subjects[i].value) {
         let newSubjects = this.state.subjects;
         newSubjects.splice(i, 1);
         this.setState({subjects: newSubjects});
@@ -74,9 +74,9 @@ class MainPage extends Component {
 
   getPrograms() {
     var subjects = {
-      "subjects": this.state.listOfFilters
+      "subjects": this.state.listOfFilters.map((filter) => filter.value)
     }
-    axios.post("https://zagsabroad-backend.herokuapp.com/filterbysubject", subjects).then((res) => {
+    axios.post("http://localhost:3001/filterbysubject", subjects).then((res) => {
       var programsToAdd = [];
       var i = 0;
       while(i < res.data.length) {
@@ -94,7 +94,18 @@ class MainPage extends Component {
           }
         }
         let programObj = {programName: programName, courses: courses};
-        programsToAdd.push(programObj);
+        // Only display programs with courses for all filters
+        let matchingProgram = true;
+        for(let j = 0; j < this.state.listOfFilters.length; j++) {
+          let hasMatchingResult = courses.some( (course) => course['guCourse'].substring(0, 4) === this.state.listOfFilters[j].value );
+          if(!hasMatchingResult) {
+            matchingProgram = false;
+            break;
+          }
+        }
+        if(matchingProgram) {
+          programsToAdd.push(programObj);
+        }
       }
       this.setState({programList: programsToAdd});
     });
@@ -108,7 +119,7 @@ class MainPage extends Component {
             placeholder = "Enter a department"
             id = "departments"
             onChange = { (selectedOption) => {
-              let newFilter = selectedOption.value;
+              let newFilter = {value: selectedOption.value, label: selectedOption.label};
               let filters = this.state.listOfFilters;
               filters.push(newFilter);
               this.setState({listOfFilters: filters});
@@ -122,10 +133,10 @@ class MainPage extends Component {
           {this.state.listOfFilters.map(filter => {
             return (
               <Chip style={{marginRight: '10px'}}
-                key={filter}
+                key={filter.value}
                 onDelete={this.handleDeleteFilter(filter)}
                 deleteIcon={<CancelIcon/>}
-                label={filter}
+                label={filter.label}
               />
             );
           })}
@@ -139,31 +150,27 @@ class MainPage extends Component {
                   <b>{program.programName}</b>
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
-                  <Typography>
-                    <div style={{textAlign: 'left'}}>
-                      <Link to={`/program/${program.programName}`}>Learn More</Link>
-                    </div>
-                    <Table>
-                      <TableHead>
-                        <TableRow>
-                          <TableCell>GU Course</TableCell>
-                          <TableCell>Host Course</TableCell>
-                          <TableCell>Requires Signature</TableCell>
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {program.courses.map((course, index) => {
-                          return (
-                            <TableRow key={index}>
-                              <TableCell>{course.guCourse}</TableCell>
-                              <TableCell>{course.hostCourse}</TableCell>
-                              <TableCell>{course.requiresSignature}</TableCell>
-                            </TableRow>
-                          );
-                        })}
-                      </TableBody>
-                    </Table>
-                  </Typography>
+                  <Link to={`/program/${program.programName}`} target="_blank">Learn More</Link>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>GU Course</TableCell>
+                        <TableCell>Host Course</TableCell>
+                        <TableCell>Requires Signature</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {program.courses.map((course, index) => {
+                        return (
+                          <TableRow key={index}>
+                            <TableCell>{course.guCourse}</TableCell>
+                            <TableCell>{course.hostCourse}</TableCell>
+                            <TableCell>{course.requiresSignature}</TableCell>
+                          </TableRow>
+                        );
+                      })}
+                    </TableBody>
+                  </Table>
                 </ExpansionPanelDetails>
               </ExpansionPanel>
             );

2bfe34104e0511cd7b0c3f49dcb59740f6ed35d6
Lucille R. Tibbetts <lucilletibbetts@gmail.com>
2019-01-10 22:48:47 -0800

    workaround to mock up mapbox-gl for tests

--- src/tests/MainPage.test.js
+++ src/tests/MainPage.test.js
@@ -3,8 +3,12 @@ import MainPage from '../MainPage.js'
 import {shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-
 configure({ adapter: new Adapter() });
+
+// Workaround to mock mapbox-gl
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+   Map: () => ({})
+}));
 
 describe('Main Page', () => {
     const mainPage = shallow(<MainPage/>);
@@ -13,8 +17,5 @@ describe('Main Page', () => {
       mainPage.setState({subjects: ['true']})
       expect(mainPage.find('#departments').props().options).toEqual(['true']);
     });
-
-
-
 
 });

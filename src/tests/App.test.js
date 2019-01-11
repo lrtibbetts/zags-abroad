755b4e89af45c19585a708abec6964c20e3ab056
Lucille R. Tibbetts <lucilletibbetts@gmail.com>
2019-01-10 22:52:02 -0800

    workaround x2

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
 
+// Workaround to mock mapbox-gl
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+   Map: () => ({})
+}));
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);

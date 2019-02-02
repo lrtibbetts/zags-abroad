73cc462becdc18feb335609caa2e27f1b4ab6cc5
Lucille R. Tibbetts <lucilletibbetts@gmail.com>
2019-02-02 13:40:28 -0800

    core filtering

--- src/ProgramDetailView.js
+++ src/ProgramDetailView.js
@@ -15,6 +15,7 @@ import CloseIcon from '@material-ui/icons/Close';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { Link } from "react-router-dom";
 
 const buttonStyle = {
@@ -27,15 +28,16 @@ class ProgramDetailView extends Component {
 
     this.state = {
       subjects: [], // Subjects in dropdown menu
-      listOfFilters : [], // Filters applied by the user
+      core: [],
+      subjectFilters : [], // Filters applied by the user
+      coreFilters: [],
       courseList : [], // Courses matching a user's search
       showMessage : false,
       message: '',
       showLogInPrompt: false,
-      photos: []
+      photos: [],
+      loading: true
     }
-
-    this.getAllCourses();
 
     axios.post("https://zagsabroad-backend.herokuapp.com/programsubjects", {"program": this.props.name}).then((res) => {
       let subjectsToAdd = [];
@@ -47,37 +49,75 @@ class ProgramDetailView extends Component {
       }
       this.setState({subjects: subjectsToAdd});
     });
+
+    axios.get("https://zagsabroad-backend.herokuapp.com/core").then((res) => {
+      let coreToAdd = [];
+      for(let i = 0; i < res.data.length; i++) {
+        let coreName = res.data[i].core_name.trim(); // Remove any white space
+        let coreObj = {value: 'core', label: coreName};
+        coreToAdd.push(coreObj);
+      }
+      this.setState({core: coreToAdd},
+          this.getAllCourses()); // Fetch all courses after state has changed
+    });
   }
-
-
 
   // Remove filter from list of filters and add back to subjects dropdown
   handleDeleteFilter = filter => () => {
-    var filters = this.state.listOfFilters;
-    for(var i = 0; i < filters.length; i++) {
-      if (filter.value === filters[i].value) {
-        filters.splice(i, 1);
-        this.setState({listOfFilters: filters});
+    if(filter.value === 'core') {
+      var filters = this.state.coreFilters;
+      for(var i = 0; i < filters.length; i++) {
+        if (filter.label === filters[i].label) { // Compare unique core label, not value
+          filters.splice(i, 1);
+          this.setState({coreFilters: filters});
+        }
       }
-    }
-    filters.length > 0 ? this.getCourses() : this.getAllCourses();
-    for(var j = 0; j < this.state.subjects.length; j++) {
-      var subjects = this.state.subjects;
-      if(filter.value < subjects[j].value) {
-        subjects.splice(j, 0, filter); // Insert at j, remove 0 items
-        this.setState({subjects: subjects});
-        return;
+      (filters.length + this.state.subjectFilters.length) > 0 ? this.getCourses() : this.getAllCourses();
+      for(var j = 0; j < this.state.core.length; j++) {
+        var core = this.state.core;
+        if(filter.label < core[j].label) {
+          core.splice(j, 0, filter); // Insert at j, remove 0 items
+          this.setState({core: core});
+          return;
+        }
+      }
+    } else {
+      filters = this.state.subjectFilters;
+      for(var k = 0; k < filters.length; k++) {
+        if (filter.value === filters[k].value) {
+          filters.splice(k, 1);
+          this.setState({subjectFilters: filters});
+        }
+      }
+      (filters.length + this.state.coreFilters.length) > 0 ? this.getCourses() : this.getAllCourses();
+      for(var m = 0; m < this.state.subjects.length; m++) {
+        var subjects = this.state.subjects;
+        if(filter.value < subjects[m].value) {
+          subjects.splice(m, 0, filter); // Insert at j, remove 0 items
+          this.setState({subjects: subjects});
+          return;
+        }
       }
     }
   }
 
-  // Remove subject from dropdown menu after it is selected
-  handleDeleteSubject(subject) {
-    for (var i = 0; i < this.state.subjects.length; i++) {
-      if (subject.value === this.state.subjects[i].value) {
-        let newSubjects = this.state.subjects;
-        newSubjects.splice(i, 1);
-        this.setState({subjects: newSubjects});
+  // Remove item from dropdown menu after it is selected
+  handleDeleteMenuItem(item) {
+    if(item.value === 'core') {
+      for (var i = 0; i < this.state.core.length; i++) {
+        if (item.label === this.state.core[i].label) { // Compare unique core label, not value
+          let newCore = this.state.core;
+          newCore.splice(i, 1);
+          this.setState({core: newCore});
+        }
+      }
+    } else {
+      for (var j = 0; j < this.state.subjects.length; j++) {
+        if (item.value === this.state.subjects[j].value) {
+          let newSubjects = this.state.subjects;
+          newSubjects.splice(j, 1);
+          this.setState({subjects: newSubjects});
+        }
       }
     }
   }
@@ -88,14 +128,15 @@ class ProgramDetailView extends Component {
     for(var i = 0; i < data.length; i++) {
       let newCourse = {guCourse: data[i].gu_course_number + ": " + data[i].gu_course_name,
         hostCourse: data[i].host_course_number ? data[i].host_course_number + ": " + data[i].host_course_name
-        : data[i].host_course_name, requiresSignature: data[i].signature_needed, id: data[i].id};
+        : data[i].host_course_name, requiresSignature: data[i].signature_needed, id: data[i].id, core: data[i].core};
       courses.push(newCourse);
     }
-    this.setState({courseList: courses});
+    this.setState({courseList: courses, loading: false});
   }
 
   // No filters, Pull all courses in program
   getAllCourses() {
+    this.setState({courseList: [], loading: true})
     axios.post("https://zagsabroad-backend.herokuapp.com/programcourses", {"program": this.props.name}).then((res) => {
       this.formatCourses(res.data);
     });
@@ -103,10 +144,11 @@ class ProgramDetailView extends Component {
 
   // Filters applied, pull matching courses in program
   getCourses() {
+    this.setState({courseList: [], loading: true})
     let params = {
       "program": this.props.name,
-      "subjects": this.state.listOfFilters.map((filter) => filter.value),
-      "core": []
+      "subjects": this.state.subjectFilters.map((filter) => filter.value),
+      "core": this.state.coreFilters.map((filter) => filter.label)
     }
     axios.post("https://zagsabroad-backend.herokuapp.com/detailsearch", params).then((res) => {
       this.formatCourses(res.data);
@@ -149,19 +191,25 @@ class ProgramDetailView extends Component {
             id = "departments"
             onChange = { (selectedOption) => {
               let newFilter = {value: selectedOption.value, label: selectedOption.label};
-              let filters = this.state.listOfFilters;
-              filters.push(newFilter);
-              this.setState({listOfFilters: filters});
-              this.handleDeleteSubject(newFilter);
+              if(this.state.searchBy === 'department') {
+                let subjFilters = this.state.subjectFilters;
+                subjFilters.push(newFilter);
+                this.setState({subjectFilters: subjFilters})
+              } else {
+                let coreFilters = this.state.coreFilters;
+                coreFilters.push(newFilter);
+                this.setState({coreFilters: coreFilters})
+              }
+              this.handleDeleteMenuItem(newFilter);
               this.getCourses();
             }}
-            options = {this.state.subjects}
+            options = {this.state.searchBy === 'department' ? this.state.subjects : this.state.core}
           />
         </div>
         <div style={{float: 'right', marginRight: '100px'}}>
         </div>
         <div style={{marginLeft: '100px'}}>
-          {this.state.listOfFilters.map(filter => {
+          {this.state.subjectFilters.map(filter => {
             return (
               <Chip style={{marginRight: '10px', marginTop: '10px'}}
                 key={filter.value}
@@ -171,27 +219,38 @@ class ProgramDetailView extends Component {
               />
             );
           })}
+          {this.state.coreFilters.map(filter => {
+            return (
+              <Chip style={{marginRight: '10px', marginTop: '10px'}}
+                key={filter.label}
+                onDelete={this.handleDeleteFilter(filter)}
+                deleteIcon={<CancelIcon/>}
+                label={filter.label}
+              />
+            );
+          })}
         </div>
-        <h2 style={{marginLeft: '100px'}}> Available Courses: </h2>
-        {this.state.listOfFilters.length > 1 && this.state.courseList.length === 0 ?
-        <p style={{marginLeft: '100px'}}> No matching courses. Try removing a filter! </p> : null}
-        <div style={{marginLeft: '100px', marginRight: '100px'}}>
+        <div style={{marginLeft: '100px', marginRight: '100px', marginTop: '20px'}}>
+        {this.state.loading ? <div id="loading">
+          <CircularProgress variant="indeterminate"/> </div>: null}
           <Table>
             <TableHead>
               <TableRow>
                 <TableCell>GU Course</TableCell>
                 <TableCell>Host Course</TableCell>
                 <TableCell>Requires Signature</TableCell>
+                <TableCell>Core Designation</TableCell>
                 <TableCell> </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {this.state.courseList.map((course) => {
+              {this.state.courseList.map((course, index) => {
                 return (
-                  <TableRow key={course.id}>
+                  <TableRow key={index}>
                     <TableCell>{course.guCourse}</TableCell>
                     <TableCell>{course.hostCourse}</TableCell>
                     <TableCell>{course.requiresSignature}</TableCell>
+                    <TableCell>{course.core}</TableCell>
                     <TableCell>{<IconButton onClick={(event) => this.saveCourse(course.id)}
                       color="primary"><AddIcon/></IconButton>}</TableCell>
                   </TableRow>

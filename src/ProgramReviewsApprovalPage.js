32bf2e9a0bbb9c90073653fec7cf1d0c9685fc5c
Lucille R. Tibbetts <lucilletibbetts@gmail.com>
2019-02-14 21:59:27 -0800

    add approval switches

--- src/ProgramReviewsApprovalPage.js
+++ src/ProgramReviewsApprovalPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Redirect } from "react-router-dom";
 import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Paper from '@material-ui/core/Paper';
 import './ProgramReviewsApprovalPage.css';
 
@@ -26,23 +27,25 @@ class ProgramReviewsApprovalPage extends Component {
         let id = review.ID;
         while(i < res.data.length && res.data[i].ID === id) {
           // Add any corresponding photos to review object
-          let width = res.data[i].width;
-          let height = res.data[i].height;
-          if (width > height && width > 400) {
-            // Landscape image: calculate scaled width and height
-            let scaledHeight = (height / width) * 400;
-            photos.push({url: res.data[i].url, height: scaledHeight, width: 400});
-          } else if (height > width && height > 350) {
-            // Portrait image: calculate scaled width and height
-            let scaledWidth = (width / height) * 350;
-            photos.push({url: res.data[i].url, height: 350, width: scaledWidth});
-          } else {
-            photos.push({url: res.data[i].url, height: res.data[i].height, width: res.data[i].width});
+          if(res.data[i].url !== null) {
+            let width = res.data[i].width;
+            let height = res.data[i].height;
+            if (width > height && width > 400) {
+              // Landscape image: calculate scaled width and height
+              let scaledHeight = (height / width) * 400;
+              photos.push({url: res.data[i].url, height: scaledHeight, width: 400});
+            } else if (height > width && height > 350) {
+              // Portrait image: calculate scaled width and height
+              let scaledWidth = (width / height) * 350;
+              photos.push({url: res.data[i].url, height: 350, width: scaledWidth});
+            } else {
+              photos.push({url: res.data[i].url, height: res.data[i].height, width: res.data[i].width});
+            }
           }
           i++;
         }
         review['photos'] = photos;
-        review = _.omit(review, ['url', 'width', 'height', 'survey_id'])
+        review = _.omit(review, ['url', 'width', 'height', 'survey_id']);
         reviewsToAdd.push(review);
       }
       this.setState({reviews: reviewsToAdd});
@@ -58,6 +61,12 @@ class ProgramReviewsApprovalPage extends Component {
             return (
               <div className="reviews" key={index}>
                 <Paper>
+                  <div style={{textAlign: 'left', marginLeft: '10px'}}>
+                    <FormControlLabel
+                      control={<Switch color="primary"> </Switch>}
+                      label="Approve">
+                    </FormControlLabel>
+                  </div>
                   <p> <b>Name:</b> {review.name} &nbsp;&nbsp; <b>Email:</b> {review.email}</p>
                   <p> <b>Major:</b> {review.major} &nbsp;&nbsp; <b>Year:</b> {review.year}</p>
                   <p> <b>Program :</b> {review.program} &nbsp;&nbsp; <b>Term:</b> {review.term}, {review.calendar_year}</p>
@@ -67,8 +76,15 @@ class ProgramReviewsApprovalPage extends Component {
                   <p> <b>Activities:</b> {review.activities}</p>
                   <p> <b>Staff:</b> {review.staff}</p>
                   <p> <b>Other:</b> {review.other}</p>
-                  {review.photos.map(photo =>
-                    <img src={photo.url} width={photo.width} height={photo.height} alt=""></img>
+                  {review.photos.length > 0 ? <h3> Photos: </h3> : null}
+                  {review.photos.map((photo, index) =>
+                    <div className="photo" key={index}>
+                      <FormControlLabel
+                        control={<Switch color="primary"> </Switch>}
+                        label="Approve">
+                      </FormControlLabel> <br/>
+                      <img src={photo.url} width={photo.width} height={photo.height} alt=""></img>
+                    </div>
                   )}
                 </Paper><br/>
               </div>

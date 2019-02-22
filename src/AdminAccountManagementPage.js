27704ed3528a0341945975cc20f4e361395c0971
Lucille R. Tibbetts <lucilletibbetts@gmail.com>
2019-02-22 11:27:32 -0800

    add snack boxes

--- src/AdminAccountManagementPage.js
+++ src/AdminAccountManagementPage.js
@@ -5,15 +5,24 @@ import Paper from '@material-ui/core/Paper';
 import Switch from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 class AdminAccountManagementPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       accounts: [],
-      loading: true
+      loading: true,
+      showMessage: false,
+      message: ''
     }
     this.loadAccounts();
+  }
+
+  displayMessage(message) {
+    this.setState({showMessage: true, message: message});
   }
 
   loadAccounts() {
@@ -30,10 +39,12 @@ class AdminAccountManagementPage extends Component {
   saveChanges(account, event) {
     if(event.target.checked) {
       axios.post("https://zagsabroad-backend.herokuapp.com/approveadmin", {"email": account.email}).then((res) => {
+        this.displayMessage(account.email + " has been given admin access");
         this.loadAccounts();
       })
     } else {
       axios.post("https://zagsabroad-backend.herokuapp.com/rejectadmin", {"email": account.email}).then((res) => {
+        this.displayMessage("Admin access has been removed for " + account.email);
         this.loadAccounts();
       })
     }
@@ -67,6 +78,24 @@ class AdminAccountManagementPage extends Component {
               </div>
             )
           })}
+          <Snackbar
+              message={this.state.message}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
+              }}
+              open={this.state.showMessage}
+              autoHideDuration={3000}
+              action={[
+                <IconButton
+                  key="close"
+                  aria-label="Close"
+                  color="inherit"
+                  onClick={(event) => this.setState({showMessage: false})}>
+                  <CloseIcon/>
+                </IconButton>,
+              ]}>
+            </Snackbar>
           {(this.state.accounts.length === 0 && !this.state.loading) ? <p> No accounts found at this time! </p> : null}
           {this.state.loading ? <CircularProgress variant="indeterminate"/> : null}
         </div>

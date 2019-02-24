0655cc93428a200372e7ed14cb99290dcce1d0b3
kburgett <kburgett@zagmail.gonzaga.edu>
2019-02-24 15:10:07 -0800

    dialog import

--- src/ProgramDetailsForm.js
+++ src/ProgramDetailsForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
 import Tooltip from '@material-ui/core/Tooltip';
 import axios from 'axios';

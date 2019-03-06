9a68464cb08b54ccb056742e39e3d3aacbaba956
Claire Norman <clairenoel97@gmail.com>
2019-03-05 16:56:01 -0800

    email verification not working

--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -82,9 +82,9 @@ class SignUpPage extends Component {
       if(res.data !== "User already exists") {
         // Account created successfully
         this.setState({accountCreated : true});
-        const cookies = this.props.cookies;
-        cookies.set('email', this.state.email, {'maxAge': 7200, 'path': '/'}); // Might be good to store user ID instead
-        cookies.set('role', 'user', {'maxAge': 7200, 'path': '/'}); // By default, users are not given admin access
+        // const cookies = this.props.cookies;
+        // cookies.set('email', this.state.email, {'maxAge': 7200, 'path': '/'}); // Might be good to store user ID instead
+        // cookies.set('role', 'user', {'maxAge': 7200, 'path': '/'}); // By default, users are not given admin access
       } else {
         this.setState({showPrompt : true});
       }

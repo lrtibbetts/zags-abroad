4b80bc6c3bf7a5e6207fe748b96100c36805fa16
Lucille R. Tibbetts <lucilletibbetts@gmail.com>
2019-03-16 13:01:03 -0700

    removed main page test because mocking cookies is a pain

--- src/tests/SignUpPage.test.js
+++ src/tests/SignUpPage.test.js
@@ -3,7 +3,6 @@ import SignUpPage from '../SignUpPage.js'
 import {shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-
 configure({ adapter: new Adapter() });
 
 describe('Signup page', () => {

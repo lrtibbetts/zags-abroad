5f51b65beb5a89488f78eb67bb2c2e2f21471c3e
Claire Norman <clairenoel97@gmail.com>
2019-04-10 15:10:16 -0700

    adding message about junk mail

--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -169,7 +169,7 @@ class SignUpPage extends Component {
           </div>
         </Dialog>
         <Dialog open={this.state.showEmailSentPrompt}>
-          <DialogTitle>Please check your email to verify your account! (also check your junk/spam folder) </DialogTitle>
+          <DialogTitle>Please check your email to verify your account! (Make sure to check you junk/spam folders too!) (also check your junk/spam folder) </DialogTitle>
           <div style={{margin: '0 auto', marginBottom: '5px'}}>
             <Button style={buttonStyle} variant="contained" component={Link} to="/">
               Okay

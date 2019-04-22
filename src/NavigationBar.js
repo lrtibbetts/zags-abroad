3100ffb269f57afb174b9e073828489805dd2ce4
Lucille R. Tibbetts <lucilletibbetts@gmail.com>
2019-04-21 17:37:23 -0700

    minor edits to faq

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -78,24 +78,25 @@ class NavigationBar extends Component {
                  Zags Abroad is a senior design project from 2018-19. The goal is to help students find a study abroad
                  program that fits their interests and fulfills academic requirements more easily!
                 </p>
-                <p style={{margin: '0 auto', marginBottom: '5px', fontWeight: 700}}> What is the "share here" link? </p>
+                <p style={{margin: '0 auto', marginBottom: '5px', fontWeight: 700}}> What is the "Share Here" link? </p>
                 <p className = "faq">
-                 The Share Here link takes you to a review form where you can provide feedback for your
-                 study abroad experience. Your responses to certain questions is visible on the program
-                 pages on our website. This allows you to share your experiences with other students and
-                 provide a more personal insight on what studying abroad is like.
+                 The Share Here link takes you to a review form where you can provide feedback about your study abroad
+                 experience. Your responses will be approved by the Study Abroad Office before being made visible on our
+                 site. Submitting a review allows you to share your experiences with other students and provide personal
+                 insight into what studying abroad is like. You can share photos, too!
                 </p>
                 <p style={{margin: '0 auto', marginBottom: '5px', fontWeight: 700}}> How should I select a program? </p>
                 <p className = "faq">
-                  Only you can know what program is best suited for your personal needs. However, our application allows you
+                  Only you can know what program is best suited to your personal needs. However, our application allows you
                   to filter through programs based on what kind of courses you may need to take so that you can find programs
-                  that fit your academic needs.
+                  that fit your academic needs. Reviews from other students can also help you learn more about What
+                  programs are actually like.
                 </p>
                 <p style={{margin: '0 auto', marginBottom: '5px', fontWeight: 700}}> What are the pluses next to course names on the program pages? </p>
                 <p className = "faq">
-                  When you click on the plus sign, it saves that course to your account. You can view your saved courses
-                  on your My Account page. They are grouped by program so that you can compare different programs easily
-                  and keep track of what you want to take during your study abroad.
+                  When you click on the plus sign, that course is saved to your account. You can view your saved courses
+                  on the My Account page. They are grouped by program so that you can compare different programs easily
+                  and keep track of what courses you want to take while abroad.
                 </p>
                 <p style={{margin: '0 auto', marginBottom: '5px', fontWeight: 700}}> What is a core designation? </p>
                 <p className = "faq">
@@ -108,12 +109,11 @@ class NavigationBar extends Component {
                 </p>
                 <p style={{margin: '0 auto', marginBottom: '5px', fontWeight: 700}}> What does ‘Requires Signature’ mean? </p>
                 <p className = "faq">
-                  A "YES" means that you must get a signature from the department chair of the course
-                  subject. For example, to get a Biology course approved, you must get a
-                  signature from the department chair of Biology.
-                  If the department type of the host course is different
-                  from that of the Gonzaga course, get the signature from the chair of the Gonzaga course department.
-                  Signature forms can be found in the study abroad office.
+                  A "YES" means that you must get a signature from the department chair of the course subject.
+                  For example, to get a biology course approved, you must get a signature from the biology department
+                  chair of Biology. If the department type of the host course is different from that of the Gonzaga
+                  course, get the signature from the chair of the Gonzaga course department. Signature forms can be
+                  found in the study abroad office.
                 </p>
                 <p style={{margin: '0 auto', marginBottom: '5px', fontWeight: 700}}> Where is Gonzaga in Florence? </p>
                 <p className = "faq">
